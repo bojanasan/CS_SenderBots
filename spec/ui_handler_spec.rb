93ace468636fe98afdcb78f0969bc59c5dee3710
Apprentice <apprentice@dbc08.devbootcamp.com>
2012-07-10 20:52:28 -0700

    yes

--- spec/ui_handler_spec.rb
+++ spec/ui_handler_spec.rb
@@ -14,10 +14,7 @@ describe "UIHandler Module" do
                                               :title => '$3244 / 1br - 789ft - Large, Modern & More-Come home to Argenta',
                                               :email => 'hzzf4-3129154467@hous.craigslist.org'})
 
-    # @listing2 = CraigslistMonitor::Listing.new({:url => 'http://sfbay.craigslist.org/sfc/apa/20934819249.html',
-    #                                              :title => '$2500 / 2br - 789ft - Large, Modern & More-Come home to Argenta',
-    #                                              :email => 'hzzf4-31239273987@hous.craigslist.org'})
-
+   
    @time_now = Time.now
     Time.stub(:now).and_return(@time_now)
     @db.execute('DELETE FROM listings')
@@ -26,7 +23,7 @@ describe "UIHandler Module" do
   it "should show a history of emails send on the users behalf" do
     time = Time.now
     @listing.store_to_db
-    # @listing2.store_to_db
+    
 
     STDOUT.should_receive(:puts).with("Title: $3244 / 1br - 789ft - Large, Modern & More-Come home to Argenta")
     STDOUT.should_receive(:puts).with("URL: http://sfbay.craigslist.org/sfc/apa/3129154467.html")
